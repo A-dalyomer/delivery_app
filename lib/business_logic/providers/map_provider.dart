53897e79e0dfa4ddceb216924acb32a548510cc4
ahmad daly <Ahmaddo1999@gmail.com>
2024-01-29 03:08:18 +0400

    completed adding documentation in the map_provider.dart

--- lib/business_logic/providers/map_provider.dart
+++ lib/business_logic/providers/map_provider.dart
@@ -13,18 +13,28 @@ class MapProvider extends ChangeNotifier {
   MapProvider() {
     getUserLocation();
   }
+
+  /// non changed controllers
   late GoogleMapController mapController;
   final TextEditingController fieldController = TextEditingController();
 
-  /// UI settings
+  /// map UI settings
   MapSelectionMode mapSelectionMode = MapSelectionMode.pickup;
   bool showingMap = true;
   bool showUserLocation = false;
+
+  /// value holders
   Set<Marker> markers = {};
   Set<Polyline> polyLines = {};
   LatLng selectedLocation = const LatLng(0, 0);
+
+  /// total trip distance
+  /// saved here to preserve not linking the order provider with the map provider which is not recommended
   double approximateDistance = 0;
 
+  /// used to initialize the map controller with a value from the map widget
+  /// assigns the initial value for the [selectedLocation]
+  /// assigns the first value on the [fieldController]
   void onMapCreated({
     required GoogleMapController controller,
     required CameraPosition initialCameraLocation,
@@ -35,16 +45,20 @@ class MapProvider extends ChangeNotifier {
     notifyListeners();
   }
 
+  /// controls the [mapSelectionMode] value which in turn changes the selection mode of the map screen
   void changeMapSelectionMode(MapSelectionMode newSelectionMode) {
     mapSelectionMode = newSelectionMode;
     notifyListeners();
   }
 
+  /// toggles the current map existence in the widget tree by changing the [showingMap] value
   void changeMapShowState(bool newState) {
     showingMap = newState;
     notifyListeners();
   }
 
+  /// updates the [selectedLocation] and [fieldController] text on map camera movement
+  /// only notifies widget listeners when the map camera stops moving
   void updateFieldController({
     required bool isIdle,
     CameraPosition? position,
@@ -63,6 +77,8 @@ class MapProvider extends ChangeNotifier {
     }
   }
 
+  /// gets user location based on device GPS
+  /// initializes the position value holders with the supplied position
   Future<void> getUserLocation() async {
     Position position = await LocationProvider.determineUserLocation();
     setCurrentSelectedLocation(LatLng(position.latitude, position.longitude));
@@ -72,6 +88,8 @@ class MapProvider extends ChangeNotifier {
     notifyListeners();
   }
 
+  /// add a marker to the [markers]
+  /// which then gets loaded on the map on the next rebuild
   Future<void> addMarker({
     required LatLng position,
     required String markerId,
@@ -89,6 +107,8 @@ class MapProvider extends ChangeNotifier {
     );
   }
 
+  /// adds the provided polyline to [polyLines]
+  /// which is shown on the map on next rebuild
   void addPolyLine(List<LatLng> polylineCoordinates) {
     PolylineId id = const PolylineId("polyline");
     Polyline polyline = Polyline(
@@ -100,11 +120,13 @@ class MapProvider extends ChangeNotifier {
     polyLines.add(polyline);
   }
 
+  /// set the value for selected location by user on the map
   void setCurrentSelectedLocation(LatLng newPickedLocation) {
     selectedLocation = newPickedLocation;
     notifyListeners();
   }
 
+  /// animate the map camera to the new position using the existing controller
   Future<void> animateCameraToPosition(LatLng newCameraPosition) async {
     mapController.animateCamera(
       CameraUpdate.newCameraPosition(
@@ -116,6 +138,13 @@ class MapProvider extends ChangeNotifier {
     );
   }
 
+  /// clears the existing markers on map
+  /// prepares new markers based on the current pickup and destination values
+  /// notify widget listeners to show the markers immediately
+  /// requests the route data
+  /// add the loaded route to the [polyLines] to be shown on the map
+  /// notify widget listener to show the new polyline on the map
+  /// calculate trip total distance in meters
   void createRouteOnMap({
     required LatLng pickLocation,
     required List<LatLng> destinations,
@@ -124,16 +153,19 @@ class MapProvider extends ChangeNotifier {
       print('getting routes of ${destinations.length} destinations');
       print(destinations);
     }
+
+    /// used variables
     List<LatLng> polylineCoordinates = [];
 
+    /// clear existing markers
     markers.clear();
 
+    /// load the new markers
     await addMarker(
       position: pickLocation,
       markerId: 'pick_up',
       mode: MapSelectionMode.pickup,
     );
-
     for (var element in destinations) {
       await addMarker(
         markerId: '${element.latitude},${element.longitude}',
@@ -142,18 +174,20 @@ class MapProvider extends ChangeNotifier {
       );
     }
     notifyListeners();
-
     if (kDebugMode) {
       print("total markers: ${markers.length}");
       print("total destinations: ${destinations.length}");
     }
 
+    /// prepare the polyline data
     MapServices mapServices = MapServices();
     polylineCoordinates = await mapServices.getRoute(
       apiMode: RouteApiMode.osrm,
       points: [pickLocation, ...destinations],
     );
     addPolyLine(polylineCoordinates);
+
+    /// calculate the total distance of the trip
     approximateDistance = await mapServices.calculateTotalDistance(
       [pickLocation, ...destinations],
     );
@@ -163,6 +197,7 @@ class MapProvider extends ChangeNotifier {
     notifyListeners();
   }
 
+  /// reset the provider state to the initial values
   void reset() {
     mapSelectionMode = MapSelectionMode.pickup;
     showingMap = true;
