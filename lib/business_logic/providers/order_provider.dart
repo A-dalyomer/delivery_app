b9dd5b79707d1412254a08dcdbe322e305c94b20
ahmad daly <Ahmaddo1999@gmail.com>
2024-01-29 03:27:48 +0400

    completed adding documentation in the order_provider.dart

--- lib/business_logic/providers/order_provider.dart
+++ lib/business_logic/providers/order_provider.dart
@@ -12,21 +12,27 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import '../constants/const_metrics.dart';
 
 class OrderProvider extends ChangeNotifier {
+  /// holds the current order step state
   OrderSteps activeStep = OrderSteps.pickingPickupLocation;
 
+  /// active order data state
   late OrderModel orderDetails;
-  bool showOrderSummaryWidget = false;
 
+  /// emits new order step state
+  /// which in turn changes the active order step widget
   void changeActiveOrderStep(OrderSteps newStep) {
     activeStep = newStep;
     notifyListeners();
   }
 
+  /// set the pick up location value in the saved order state
   void setPickupLocation(LatLng pickupLocation) {
     orderDetails = OrderModel(pickupLocation: pickupLocation);
     addMoreDestinations();
   }
 
+  /// adds new destination to the saved order state
+  /// then changes the order step to the next one
   void addNewDestination(LatLng newDestination) {
     if (orderDetails.destinations.isEmpty) {
       orderDetails.destinations.add(newDestination);
@@ -43,6 +49,9 @@ class OrderProvider extends ChangeNotifier {
     }
   }
 
+  /// removes a specified destination from the saved destinations in the saved state
+  /// then notifies the listening widget if any left
+  /// if none left it cancels the active order removing all the saved state data
   void removeDestination(BuildContext context, LatLng removedDestination) {
     orderDetails.destinations.remove(removedDestination);
     if (orderDetails.destinations.isEmpty) {
@@ -52,6 +61,8 @@ class OrderProvider extends ChangeNotifier {
     }
   }
 
+  /// set the value for receiver info in the saved state
+  /// then changes the active order step to the next one
   void setReceiverInfo({
     required String name,
     required String phoneNumber,
@@ -67,16 +78,21 @@ class OrderProvider extends ChangeNotifier {
     );
   }
 
+  /// changes the order step back to the destination picker
   void addMoreDestinations() {
     changeActiveOrderStep(
       OrderSteps.pickingDestination,
     );
   }
 
+  /// changes the order step to the next one as confirming the order
   void confirmOrder() {
     changeActiveOrderStep(OrderSteps.rideNow);
   }
 
+  /// checks if goods type is selected
+  /// if selected, it goes to next step which is [startRideSelection]
+  /// is not selected, it starts the goods selection process
   void rideConfirmation(BuildContext context) {
     if (orderDetails.goodsType.isEmpty) {
       setGoodsDetails(context);
@@ -85,6 +101,10 @@ class OrderProvider extends ChangeNotifier {
     }
   }
 
+  /// sets the goods type in the saved order state
+  /// which involves navigating to the [GoodsSelectionScreen]
+  /// then receiving the resulted value from the screen
+  /// then saves the result in the saved order state
   void setGoodsDetails(BuildContext context) async {
     String? selectedGoods = await Navigator.push(
       context,
@@ -96,21 +116,25 @@ class OrderProvider extends ChangeNotifier {
     }
   }
 
+  /// set the ride type in the saved order state and notified the listening widget
   void setRideType(RideType selectedRide) {
     orderDetails.rideType = selectedRide;
     notifyListeners();
   }
 
+  /// set the offered price in the saved order state
   void setOfferedPrice(double setPrice) {
     orderDetails.offeredPrice = setPrice;
     notifyListeners();
   }
 
+  /// set the recommended fare value in the saved order state
   void setRecommendedFare(double setPrice) {
     orderDetails.recommendedFare = double.parse(setPrice.toStringAsFixed(2));
     notifyListeners();
   }
 
+  /// shows the order summary dialog with the associated logic
   void startRideSelection(BuildContext context) {
     double recommendedFare = orderDetails.recommendedFare;
     RideType rideType = orderDetails.rideType;
@@ -133,7 +157,10 @@ class OrderProvider extends ChangeNotifier {
               print('offered fare: ${fairController.text}');
             }
             if (fairController.text.isNotEmpty) {
+              /// set the offered price by user from the text field
               setOfferedPrice(double.parse(fairController.text));
+
+              /// changes the active order step to the next one
               changeActiveOrderStep(OrderSteps.waitingOffer);
             }
           },
@@ -142,6 +169,9 @@ class OrderProvider extends ChangeNotifier {
     );
   }
 
+  /// handles the logic of back button in the order progress
+  /// if not on the second step, it changes to one step back
+  /// if on the second step, it cancels the active order
   void backInOrderSteps(BuildContext context) {
     if (activeStep.index > 1) {
       changeActiveOrderStep(OrderSteps.values[activeStep.index - 1]);
@@ -150,6 +180,7 @@ class OrderProvider extends ChangeNotifier {
     }
   }
 
+  /// cancel the active order and reset the state values
   void cancelOrder() {
     activeStep = OrderSteps.pickingPickupLocation;
     orderDetails.resetOrder();
